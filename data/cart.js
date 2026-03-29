25b32d216bb4484197613d216211b67e6c1f122b
HiteshTomar2004 <hitesh28tomar@gmail.com>
2026-03-29 23:53:13 +0530

    Save cart in localStorage

--- data/cart.js
+++ data/cart.js
@@ -1,10 +1,16 @@
-export let cart = [{
+export let cart = JSON.parse(localStorage.getItem('cart')) ||
+[{
     productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
     quantity: 2
 },{
     productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
     quantity: 1
 }];
+
+export function saveToLocalStorage(){
+    localStorage.setItem('cart',JSON.stringify(cart));
+}
+
 
 let totalQuantity = 0;
 
@@ -28,6 +34,8 @@ export function addToCart(productId){
             quantity
         });
     }
+
+    saveToLocalStorage();
 }
 
 export function totalQuantityCalculator(){
@@ -48,4 +56,6 @@ export function deleteFromCart(productId){
         }
     });
     cart = newCart;
+
+    saveToLocalStorage();
 }
