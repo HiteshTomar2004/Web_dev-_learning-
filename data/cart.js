b152fcd04227277829748179fbdaaf8be206d698
HiteshTomar2004 <hitesh28tomar@gmail.com>
2026-03-29 23:30:54 +0530

    implemented delete from cart

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,4 @@
-export const cart = [{
+export let cart = [{
     productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
     quantity: 2
 },{
@@ -38,3 +38,14 @@ export function totalQuantityCalculator(){
     document.querySelector('.js-cart-icon-update').innerHTML = totalQuantity;
 }
 
+
+export function deleteFromCart(productId){
+    const newCart = [];
+
+    cart.forEach((cartItem)=>{
+        if(cartItem.productId !== productId){
+            newCart.push(cartItem);
+        }
+    });
+    cart = newCart;
+}
