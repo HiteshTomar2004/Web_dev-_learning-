62e5bf6b43383d6f6f32c4eb61a65e31aaae26de
HiteshTomar2004 <hitesh28tomar@gmail.com>
2026-04-05 19:30:52 +0530

    loadProducts() beforeEach() hook

--- Amazon-project/tests/checkout/orderSummarytest.js
+++ Amazon-project/tests/checkout/orderSummarytest.js
@@ -1,4 +1,5 @@
 import {cart, loadFromStorage} from "../../data/cart.js";
+import { loadProducts } from "../../data/products.js";
 import { renderOrderSummary } from "../../scripts/checkout/orderSummary.js"
 
 //Integrated testing
@@ -7,7 +8,13 @@ describe('test suite: renderOrderSummary',()=>{//testing how page looks and beha
     const productId1 = `e43638ce-6aa0-4b85-b27f-e1d07eb678c6`;
     const productId2 = `15b6fc6f-327a-4ec4-896f-486349e85a3d`;
 
-
+    beforeAll(//done is a parameter provided by jasmine to wait before next step
+        (done)=>{
+            loadProducts(()=>{
+                done();//done can be used inside beforeEach and it too
+            });
+        }
+    );
     //some hooks:- beforeEach() runs before each test afterEach() runs after each test beforeAll() before all tests afterAll() runs after all tests
     beforeEach(()=>{//beforeEach is a hook and allows to run this funciton before each test
         
