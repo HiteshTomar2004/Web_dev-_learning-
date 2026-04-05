d4fc2b92e8373e0fc7824190d1ee6af4c5ad2668
HiteshTomar2004 <hitesh28tomar@gmail.com>
2026-04-06 00:21:12 +0530

    Add URL parameters

--- Amazon-project/data/orders.js
+++ Amazon-project/data/orders.js
@@ -8,3 +8,15 @@ export function addOrder(order){
 function saveToStorage(){
     localStorage.setItem('orders',JSON.stringify(orders));
 }
+
+/*URL parameters also called search parameters
+let us save data directly in the URL
+eg-  tracking.html?orderId=123
+? mmeans we are adding a url parameter
+it is a property value pair left side is property right side is value
+so ?orderId=123 can be represented in an object like this
+{
+    orderId: 123
+}
+this data is saved directly in url and we can use JS to get it out
+*/
